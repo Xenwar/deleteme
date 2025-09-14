4e97cc7f8d8f2329bd8ab937d62a9eaf1f7aba87
Anwar Hassen <12200849+Xenwar@users.noreply.github.com>
2025-09-14 07:58:56 +0000

    v12

---
+++ .github/workflows/ansible_2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -l
+
+echo "We are installing requirement.txt"
+echo "We are installing other apps"
+echo "We are running ansibble group 2"
---
+++ .github/workflows/terraform_net.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -l
+
+echo "We are installing requirement.txt"
+echo "We are installing other apps"
+echo "We are creating network resources"
